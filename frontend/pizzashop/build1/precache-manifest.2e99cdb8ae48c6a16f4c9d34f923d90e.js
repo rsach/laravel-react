45053d26e6ab40c3ff0ea86ad1b318d47400b237
Rahul Sachdeva <rahul.sachdeva@live.com>
2020-01-02 00:43:09 +0530

    build

---
+++ frontend/pizzashop/build1/precache-manifest.2e99cdb8ae48c6a16f4c9d34f923d90e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efbd4664bea0307ee66306ba4699b5fb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2d3df5053df2671fbca4",
+    "url": "/static/css/main.0845e2e9.chunk.css"
+  },
+  {
+    "revision": "c33afb191f7e52aeff97",
+    "url": "/static/js/2.fffc10c1.chunk.js"
+  },
+  {
+    "revision": "affe81ef46457fdd1d72fa3e61b9bed4",
+    "url": "/static/js/2.fffc10c1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2d3df5053df2671fbca4",
+    "url": "/static/js/main.a8c9c8ab.chunk.js"
+  },
+  {
+    "revision": "bd21890ffc042b2a9f3a",
+    "url": "/static/js/runtime-main.3822e7b8.js"
+  }
+]);
